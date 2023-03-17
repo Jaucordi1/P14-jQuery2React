c1424a8bb8d76293220c654ec727e2c0ae0e5a27
Jason Aucordier <jaucordi@gmail.com>
2023-03-17 14:02:52 +0100

    Creating `Navigation` react component

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,9 @@
+import Classes from './Navigation.module.scss'
+
+export function Navigation() {
+    return (
+        <nav className={Classes.nav}>
+            <h1>HRNet</h1>
+        </nav>
+    )
+}
