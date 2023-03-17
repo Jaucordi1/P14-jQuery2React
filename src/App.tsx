af46620460630e30799812999efad90f433967d0
Jason Aucordier <jaucordi@gmail.com>
2023-03-17 14:03:30 +0100

    Adding `Navigation` in `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.scss'
+import React from 'react'
+import {Navigation} from './components/navigation/Navigation'
 
 function App() {
     return (
         <div className="App">
-            <h1>HRNet</h1>
+            <Navigation />
         </div>
     )
 }
