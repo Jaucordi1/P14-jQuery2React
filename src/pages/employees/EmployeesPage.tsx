ac7820e802e32f6e0e13818ee9f13329eb1d25d0
Jason Aucordier <jaucordi@gmail.com>
2023-03-17 15:23:28 +0100

    Adding EmployeesPage

---
+++ src/pages/employees/EmployeesPage.tsx
@@ -0,0 +1,9 @@
+import Classes from './Employees.module.scss'
+
+export default function EmployeesPage() {
+    return (
+        <main className={Classes.page}>
+            <h1>Current Employees</h1>
+        </main>
+    )
+}
