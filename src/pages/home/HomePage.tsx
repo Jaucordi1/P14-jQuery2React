c6cb53f09690f75d70f414d2e7d72156dbd588e1
Jason Aucordier <jaucordi@gmail.com>
2023-03-17 17:22:36 +0100

    WIP HomePage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,59 +1,60 @@
 import Classes from './HomePage.module.scss'
+import {Dropdown, DropdownItem} from "../../components/Dropdown/Dropdown";
+import {Fieldset} from "../../components/forms/Fieldset/Fieldset";
+import {Input} from "../../components/forms/Input/Input";
+import {Button} from "../../components/Button/Button";
+import {Title} from "../../components/Title/Title";
+import React from "react";
+
+const STATES: DropdownItem[] = [
+    {
+        name: 'Alabama',
+        value: 'AL',
+    },
+]
+const DEPARTMENTS: DropdownItem[] = [
+    {
+        name: 'Sales',
+        value: 'sales',
+    },
+    {
+        name: 'Marketing',
+        value: 'marketing',
+    },
+    {
+        name: 'Engineering',
+        value: 'engineering',
+    },
+    {
+        name: 'Human Resources',
+        value: 'hr',
+    },
+    {
+        name: 'Legal',
+        value: 'legal',
+    },
+]
 
 export default function HomePage() {
     return (
         <main className={Classes.page}>
-            <h1>HRnet</h1>
-            <h2>Create Employee</h2>
+            <Title type="h1">HRnet</Title>
+            <Title type="h2">Create Employee</Title>
 
-            <form action="#">
-                <label>
-                    <span>First Name</span>
-                    <input type="text" name="firstname" autoComplete="firstname" />
-                </label>
-                <label>
-                    <span>Last Name</span>
-                    <input type="text" name="lastname" autoComplete="lastname" />
-                </label>
-                <label>
-                    <span>Date of Birth</span>
-                    <input type="date" name="birthdate" />
-                </label>
-                <label>
-                    <span>Start Date</span>
-                    <input type="date" name="startdate" />
-                </label>
-                <fieldset>
-                    <legend>Address</legend>
-                    <label>
-                        <span>Street</span>
-                        <input type="text" name="street" autoComplete="street" />
-                    </label>
-                    <label>
-                        <span>City</span>
-                        <input type="text" name="City" autoComplete="city" />
-                    </label>
-                    <label>
-                        <span>State</span>
-                        <select name="state">
-                            <option value="AL">Alabama</option>
-                        </select>
-                    </label>
-                    <label>
-                        <span>Zip code</span>
-                        <input type="text" name="zipcode" autoComplete="zipcode" />
-                    </label>
-                </fieldset>
-                <label>
-                    <span>Department</span>
-                    <select name="department">
-                        <option value="sales">Sales</option>
-                        <option value="marketing">Marketing</option>
-                        <option value="engineering">Engineering</option>
-                        <option value="hr">Human Resources</option>
-                        <option value="legal">Legal</option>
-                    </select>
-                </label>
+            <form action="#" className={Classes.form}>
+                <Input id="firstname" label="First Name" type="text" name="firstname" autoComplete="firstname" />
+                <Input id="lastname" label="Last Name" type="text" name="lastname" autoComplete="lastname" />
+                <Input id="birthdate" label="Date of Birth" type="date" name="birthdate" />
+                <Input id="start-date" label="Start Date" type="date" name="start-date" />
+                <Fieldset legend="Address">
+                    <Input id="street" label="Street" type="text" name="street" autoComplete="street" />
+                    <Input id="city" label="City" type="text" name="city" autoComplete="city" />
+                    <Dropdown id="state" label="State" name="state" items={STATES} />
+                    <Input id="zipcode" label="Zip Code" type="number" name="zipcode" autoComplete="zipcode" min={0} />
+                </Fieldset>
+                <Dropdown label="Department" items={DEPARTMENTS} id="department" name="department" />
+
+                <Button type="submit" className={Classes.button}>Save</Button>
             </form>
         </main>
     )
