82984e0bbeac8ec48233842522d236f013b448cc
Jason Aucordier <jaucordi@gmail.com>
2023-05-05 20:41:40 +0200

    Adding deserializeEmployee when selecting employees from store to reconstruct Date objects

--- src/store/features/employees/selectors.ts
+++ src/store/features/employees/selectors.ts
@@ -1,10 +1,11 @@
 import type {RootState} from "../../index";
 import adapter, {selectEmployeeID} from './adapter';
 import mapper from "../../../data/database/mapper/Mapper";
+import {deserializeEmployee} from "../../../data/models/employee/IEmployee";
 
 const {selectById, selectAll, selectIds} = adapter.getSelectors();
 
-export const selectEmployees = (state: RootState) => selectAll(state.employees).map(mapper.mapEmployee);
+export const selectEmployees = (state: RootState) => selectAll(state.employees).map(deserializeEmployee).map(mapper.mapEmployee);
 export const selectEmployee = (state: RootState, id: ReturnType<typeof selectEmployeeID>) => {
     const found = selectById(state.employees, id);
     return found
