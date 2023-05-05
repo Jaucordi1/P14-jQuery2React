1180ec75c217c5fadcea2630f2a822bede4a1511
Jason Aucordier <jaucordi@gmail.com>
2023-05-05 20:43:15 +0200

    Cleaning HomePage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,141 +1,192 @@
-import Classes from './HomePage.module.scss'
-import {Dropdown, DropdownItem} from "../../components/Dropdown/Dropdown";
-import {Title} from "../../components/Title/Title";
+import Classes from "./HomePage.module.scss";
+import type {IEmployee} from "../../data/models/employee/IEmployee";
+import React from "react";
+import * as yup from "yup";
+import Modal from "react-modal";
 import {useAppDispatch} from "../../store";
 import {employeeActions} from "../../store/features/employees";
-import {Formik} from "formik";
-import * as yup from 'yup';
-import React from "react";
-import {Input} from "../../components/forms/Input/Input";
+import {Field, Form, Formik, FormikHelpers} from "formik";
+import {Title} from "../../components/Title/Title";
+import {Dropdown, DropdownItem} from "../../components/forms/Dropdown/Dropdown";
+import {TextField} from "../../components/forms/TextField/TextField";
 import {Fieldset} from "../../components/forms/Fieldset/Fieldset";
 import {Button} from "../../components/Button/Button";
-import {IEmployee} from "../../data/models/employee/IEmployee";
+import {DateField} from "../../components/forms/DateField/DateField";
 
 const STATES: DropdownItem[] = [
     {
-        name: 'Choose a state',
-        value: '',
+        label: "Choose a state",
+        value: "",
     },
     {
-        name: 'Alabama',
-        value: 'AL',
+        label: "Alabama",
+        value: "AL",
     },
-]
+];
 const DEPARTMENTS: DropdownItem[] = [
     {
-        name: 'Sales',
-        value: 'sales',
+        label: "Choose a department",
+        value: "",
     },
     {
-        name: 'Marketing',
-        value: 'marketing',
+        label: "Sales",
+        value: "sales",
     },
     {
-        name: 'Engineering',
-        value: 'engineering',
+        label: "Marketing",
+        value: "marketing",
     },
     {
-        name: 'Human Resources',
-        value: 'hr',
+        label: "Engineering",
+        value: "engineering",
     },
     {
-        name: 'Legal',
-        value: 'legal',
+        label: "Human Resources",
+        value: "hr",
     },
-]
+    {
+        label: "Legal",
+        value: "legal",
+    },
+];
+
+const now = new Date();
 
 const validationSchema = yup.object({
     id: yup.number(),
     personalInformation: yup.object({
-        firstname: yup.string().required('Employee first name is mandatory.'),
-        lastname: yup.string().required('Employee last name is mandatory.'),
+        firstname: yup.string().required("Employee first name is mandatory."),
+        lastname: yup.string().required("Employee last name is mandatory."),
         address: yup.object({
-            street: yup.string().required('Street name and number are mandatory.'),
-            city: yup.string().required('City is mandatory.'),
-            state: yup.string().oneOf(STATES.map(state => state.value)).required('State is mandatory.'),
-            zipcode: yup.string().required('Zipcode is mandatory.'),
-        }).required('Employee address is mandatory.'),
-        birthdate: yup.date().required('Employee birthdate is mandatory.'),
-    }).required('Employee personal information are mandatory.'),
-    startDate: yup.date().required('Employee start date is mandatory.'),
-    department: yup.string().oneOf(DEPARTMENTS.map(dep => dep.value)).required('Employee first name is mandatory.'),
+            street: yup.string().required("Street number and name are mandatory."),
+            city: yup.string().required("City is mandatory."),
+            state: yup.string()
+                .oneOf(STATES.map(state => state.value).filter(v => v.length > 0))
+                .required("State is mandatory."),
+            zipcode: yup.string().required("Zip code is mandatory."),
+        }).required(),
+        birthdate: yup.date().required("Employee birthdate is mandatory.").max(now),
+    }).required(),
+    startDate: yup.date().required("Employee start date is mandatory."),
+    department: yup.string()
+        .oneOf(DEPARTMENTS.map(dep => dep.value).filter(v => v.length > 0))
+        .required("Employee department is mandatory."),
 });
 
 function leadingZero(num: number): string {
     return num < 10 ? `0${num}` : num.toString(10);
 }
 
+Modal.setAppElement("#root");
+const modalStyles = {
+    overlay: {
+        backgroundColor: "rgba(0, 0, 0, 0.75)",
+    },
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        overflow: "visible",
+    },
+};
+
 export default function HomePage() {
     const dispatch = useAppDispatch();
-    const dateToString = (date: Date) => date.toLocaleDateString('fr-CA', {year: 'numeric', month: '2-digit', day: '2-digit'})
+    const [modalOpened, setModalOpened] = React.useState(false);
+    const closeModal = () => setModalOpened(false);
+    const dateToString = (date: Date) => date.toLocaleDateString("fr-CA", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+    });
+    const handleSubmit = async (values: Omit<IEmployee, "id">, formikHelpers: FormikHelpers<Omit<IEmployee, "id">>) => {
+        formikHelpers.setSubmitting(true);
+        setModalOpened(true);
+        await dispatch(employeeActions.CREATE_EMPLOYEE_ACTION(values as Omit<IEmployee, "id">));
+        formikHelpers.setSubmitting(false);
+    };
 
     return (
         <main className={Classes.page}>
             <Title type="h1">HRnet</Title>
             <Title type="h2">Create Employee</Title>
 
-            <Formik<Omit<IEmployee, 'id'>> validationSchema={validationSchema}
-                    initialValues={{
-                        personalInformation: {
-                            firstname: '',
-                            lastname: '',
-                            address: {
-                                street: '',
-                                city: '',
-                                state: '',
-                                zipcode: '',
-                            },
-                            birthdate: new Date(),
-                        },
-                        department: 'sales',
-                        startDate: new Date(),
-                    }}
-                    onSubmit={async (values, formikHelpers) => {
-                        formikHelpers.setSubmitting(true);
-                        await dispatch(employeeActions.CREATE_EMPLOYEE_ACTION(values));
-                        formikHelpers.setSubmitting(false);
-                    }}>
-                {({values, errors, handleChange, setFieldValue, handleSubmit}) => {
-                    if (Object.keys(errors).length > 0) {
-                        console.debug('ERRORS :', errors);
-                    }
+            <Formik<Omit<IEmployee, "id">> validationSchema={validationSchema} initialValues={{
+                personalInformation: {
+                    firstname: "",
+                    lastname: "",
+                    address: {
+                        street: "",
+                        city: "",
+                        state: "",
+                        zipcode: "",
+                    },
+                    birthdate: now,
+                },
+                department: "",
+                startDate: new Date(),
+            }} onSubmit={handleSubmit}>
+                {({
+                      values,
+                      touched,
+                      errors,
+                      handleChange,
+                      setFieldValue,
+                      handleSubmit,
+                      isValidating,
+                      isSubmitting,
+                      submitCount,
+                      resetForm,
+                  }) => {
                     return (
-                        <form className={Classes.form} onSubmit={handleSubmit}>
-                            <Input id="firstname" label="First Name" type="text" name="firstname" autoComplete="firstname"
-                                   value={values.personalInformation.firstname}
-                                   onChange={event => setFieldValue('personalInformation.firstname', event.target.value)} />
-                            <Input id="lastname" label="Last Name" type="text" name="lastname" autoComplete="lastname"
-                                   value={values.personalInformation.lastname}
-                                   onChange={event => setFieldValue('personalInformation.lastname', event.target.value)} />
-                            <Input id="birthdate" label="Date of Birth" type="date" name="birthdate"
-                                   value={dateToString(values.personalInformation.birthdate)}
-                                   onChange={event => setFieldValue('personalInformation.birthdate', new Date(event.target.value))} />
-                            <Input id="start-date" label="Start Date" type="date" name="start-date"
-                                   value={dateToString(values.startDate)}
-                                   onChange={event => setFieldValue('startDate', new Date(event.target.value))} />
+                        <Form className={Classes.form} onSubmit={handleSubmit}>
+                            <Modal isOpen={!isValidating && modalOpened} onRequestClose={closeModal}
+                                   onAfterClose={resetForm} style={modalStyles}
+                                   shouldCloseOnEsc={true} shouldReturnFocusAfterClose={true}
+                                   shouldCloseOnOverlayClick={true}>
+                                <button onClick={closeModal} className={Classes.modalCloseBtn}>&times;</button>
+                                {isSubmitting && "Creating resource…"}
+                                {!isSubmitting && "Resource successfully created!"}
+                            </Modal>
+
+                            {/*<Field as={Input} name="personalInformation.firstname" label="First Name" placeholder="John" />*/}
+
+                            <Field as={TextField} name="personalInformation.firstname" autoComplete="firstname"
+                                   label="First Name"
+                                   placeholder="John" />
+
+                            <Field as={TextField} name="personalInformation.lastname" autoComplete="lastname"
+                                   label="Last Name"
+                                   placeholder="Doe" />
+
+                            <Field as={DateField} name="personalInformation.birthdate" autoComplete="birthdate"
+                                   label="Date of Birth" />
+
+                            <Field as={DateField} name="startDate" label="Start Date" />
+
                             <Fieldset legend="Address">
-                                <Input id="street" label="Street" type="text" name="street" autoComplete="street"
-                                       value={values.personalInformation.address.street}
-                                       onChange={event => setFieldValue('personalInformation.address.street', event.target.value)} />
-                                <Input id="city" label="City" type="text" name="city" autoComplete="city"
-                                       value={values.personalInformation.address.city}
-                                       onChange={event => setFieldValue('personalInformation.address.city', event.target.value)} />
-                                <Dropdown id="state" label="State" name="state" items={STATES}
-                                          value={values.personalInformation.address.state}
-                                          onChange={event => setFieldValue('personalInformation.address.state', event.target.value)} />
-                                <Input id="zipcode" label="Zip Code" type="number" name="zipcode" autoComplete="zipcode"
-                                       min={0} value={values.personalInformation.address.zipcode}
-                                       onChange={event => setFieldValue('personalInformation.address.zipcode', event.target.value)} />
+                                <Field as={TextField} name="personalInformation.address.street" autoComplete="street"
+                                       label="Street" placeholder="123…" />
+
+                                <Field as={TextField} name="personalInformation.address.city" autoComplete="city"
+                                       label="City" placeholder="Paris" />
+
+                                <Dropdown label="State" options={STATES} name="personalInformation.address.state" />
+
+                                <Field as={TextField} name="personalInformation.address.zipcode" autoComplete="zipcode"
+                                       label="Zip Code" placeholder="64000" />
                             </Fieldset>
-                            <Dropdown label="Department" items={DEPARTMENTS} id="department" name="department"
-                                      value={values.department}
-                                      onChange={event => setFieldValue('department', event.target.value)} />
+
+                            <Dropdown label="Department" options={DEPARTMENTS} name="department" />
 
                             <Button type="submit" className={Classes.button}>Save</Button>
-                        </form>
+                        </Form>
                     );
                 }}
             </Formik>
         </main>
-    )
+    );
 }
